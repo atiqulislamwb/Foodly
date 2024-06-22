1e73afd4d59701f4776de6c4aee8d16d9773ce30
atiqulislamwb <atiqul@lintechgroup.co.uk>
2024-06-22 11:42:07 +0600

    initail project setup

---
+++ Foodly/FoodlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodlyApp.swift
+//  Foodly
+//
+//  Created by Atiqul Islam on 22/6/24.
+//
+
+import SwiftUI
+
+@main
+struct FoodlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
